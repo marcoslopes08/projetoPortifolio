e4fc2a79555dcd6b224552da809a5385c79d6e19
marcoslopes08 <112578905+marcoslopes08@users.noreply.github.com>
2022-09-02 17:23:24 -0300

    commit inicial
    Projeto para portifólio em Front-End

---
+++ java/carrossel.js
@@ -0,0 +1,12 @@
+
+/* A variável 'counter' começa em 1, então sempre que 'counter for MENOR que 4 (pois são 4 Fotos), 
+counter volta pro incio e vira um Looping (chega no Final, volta pro começo e isso sempre se repete) */
+type="text/javascript"
+    var counter = 1;
+    setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+    }, 5000);
