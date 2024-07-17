7eb251921d03bbef67c74481ca11456b75f53d6e
Marcos Lopes <112578905+marcoslopes08@users.noreply.github.com>
2024-07-17 11:50:19 -0300

    Update carrossel.js
    Atualização Julho de 2024

--- java/carrossel.js
+++ java/carrossel.js
@@ -1,7 +1,6 @@
-
 /* A variável 'counter' começa em 1, então sempre que 'counter for MENOR que 4 (pois são 4 Fotos), 
 counter volta pro incio e vira um Looping (chega no Final, volta pro começo e isso sempre se repete) */
-/* o tempo de rolagem de cada imagem era de 5000, aumentei para 7 segundos */
+/* o tempo de rolagem de cada imagem era de 7000, dimuní para 5 segundos */
 type="text/javascript"
     var counter = 1;
     setInterval(function () {
@@ -10,4 +9,4 @@ type="text/javascript"
     if (counter > 4) {
         counter = 1;
     }
-    }, 7000);
+    }, 5000);
