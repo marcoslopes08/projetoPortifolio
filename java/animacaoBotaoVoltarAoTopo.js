5d631a401fa93d23e0290638cdb0b1e11ffaf977
Marcos Lopes <112578905+marcoslopes08@users.noreply.github.com>
2024-09-04 14:58:52 -0300

    Update animacaoBotaoVoltarAoTopo.js
    Alteração - Botão Voltar ao Topo só aparecerá a partir da rolagem de "Section 3 - Habilidades" "id=service"

--- java/animacaoBotaoVoltarAoTopo.js
+++ java/animacaoBotaoVoltarAoTopo.js
@@ -1,5 +1,29 @@
 /* jQuery que faz o Botão Voltar ao Topo aparecer/desaparecer durante a rolagem na página */
 jQuery(document).ready(function() {
+    var offset = 300;
+    var duration = 500;
+    var $serviceSection = jQuery('#service');
+    var serviceSectionTop = $serviceSection.offset().top;
+
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > serviceSectionTop + offset) {
+            jQuery('.voltarAoTopo').fadeIn(duration);
+        } else {
+            jQuery('.voltarAoTopo').fadeOut(duration);
+        }
+    });
+
+    jQuery('.voltarAoTopo').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+});
+
+
+/* Antigo Botão - foi melhorado pelo Gemini, pois o Botão já aparecia assim
+que eu entrava no site 
+jQuery(document).ready(function() {
 	
 	var offset = 300;
 	var duration = 500;
@@ -18,4 +42,4 @@ jQuery(document).ready(function() {
 	return false;
 	});
 });
-
+*/
